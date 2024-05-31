bbed595d391baff1f67745b036abf987a496294f
QuentinGra <pilou4260042@gmail.com>
2024-05-31 12:03:37 +0200

    feat(gender): add create

---
+++ src/Controller/Backend/GenderController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller\Backend;
+
+use App\Entity\Gender;
+use App\Form\GenderType;
+use App\Repository\GenderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin/genders', name: 'admin.genders')]
+class GenderController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+    ) {
+    }
+
+    #[Route('', name: '.index', methods: ['GET'])]
+    public function index(GenderRepository $genderRepo): Response
+    {
+        return $this->render('Backend/Gender/index.html.twig', [
+            'genders' => $genderRepo->findAll(),
+        ]);
+    }
+
+    #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response | RedirectResponse
+    {
+        $gender = new Gender();
+
+        $form = $this->createForm(GenderType::class, $gender);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($gender);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Genre crée avec succès');
+
+            return $this->redirectToRoute('admin.genders.index');
+        }
+
+        return $this->render('Backend/Gender/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: '.edit', methods: ['GET', 'POST'])]
+    public function update(?Gender $gender, Request $request): Response | RedirectResponse
+    {
+    }
+}
