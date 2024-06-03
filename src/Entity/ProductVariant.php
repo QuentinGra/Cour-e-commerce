d27b72e1096c090dec979e29d748952d34ba2372
QuentinGra <pilou4260042@gmail.com>
2024-06-03 09:43:15 +0200

    feat(productVariant): add entity and constraint

---
+++ src/Entity/ProductVariant.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductVariantRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ProductVariantRepository::class)]
+class ProductVariant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private ?float $size = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSize(): ?float
+    {
+        return $this->size;
+    }
+
+    public function setSize(float $size): static
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+}
