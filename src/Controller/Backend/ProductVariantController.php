cdc9506ae5925fb125ce49fccd078cb04bc17a46
QuentinGra <pilou4260042@gmail.com>
2024-06-03 11:23:12 +0200

    feat(productVariant): add read

--- src/Controller/Backend/ProductVariantController.php
+++ src/Controller/Backend/ProductVariantController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\Backend;
 
-use App\Repository\ProductVariantRepository;
+use App\Entity\Product;
+use App\Entity\ProductVariant;
+use App\Form\ProductVariantType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/admin/productVariant', name: 'admin.productVariant')]
 class ProductVariantController extends AbstractController
@@ -16,11 +20,86 @@ class ProductVariantController extends AbstractController
     ) {
     }
 
-    #[Route('/{id}', name: '', mehtods: ['GET'])]
-    public function index(ProductVariantRepository $productVariantRepo): Response
+    #[Route('/{id}', name: '.index', methods: ['GET'])]
+    public function index(?Product $product): Response
     {
-        return $this->render('backend/product_variant/index.html.twig', [
-            'productVariants' => $productVariantRepo->findAll(),
+        if (!$product) {
+            $this->addFlash('error', 'Product Not Found');
+
+            return $this->redirectToRoute('admin.products.index');
+        }
+
+        return $this->render('Backend/ProductVariant/index.html.twig', [
+            'productVariants' => $product->getProductVariants(),
         ]);
+    }
+
+    #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response | RedirectResponse
+    {
+        $productVariant = new ProductVariant();
+
+        $form = $this->createForm(ProductVariantType::class, $productVariant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($productVariant);
+            $this->em->flush();
+
+            $this->addFlash('success', 'ProductVariant crée avec succès');
+
+            return $this->redirectToRoute('admin.productVariant.index');
+        }
+
+        return $this->render('Backend/ProductVariant/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: '.edit', methods: ['GET', 'POST'])]
+    public function update(?ProductVariant $productVariant, Request $request): Response | RedirectResponse
+    {
+        if (!$productVariant) {
+            $this->addFlash('error', 'ProductVariant Not Found');
+
+            return $this->redirectToRoute('admin.productVariant.index');
+        }
+
+        $form = $this->createForm(ProductVariantType::class, $productVariant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($productVariant);
+            $this->em->flush();
+
+            $this->addFlash('success', 'ProductVariant modifié avec succès');
+
+            return $this->redirectToRoute('admin.productVariant.index');
+        }
+
+        return $this->render('Backend/ProductVariant/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: '.delete', methods: ['POST'])]
+    public function delete(?ProductVariant $productVariant, Request $request): Response|RedirectResponse
+    {
+        if (!$productVariant) {
+            $this->addFlash('error', 'ProductVariant Not Found');
+
+            return $this->redirectToRoute('admin.productVariant.index');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $productVariant->getId(), $request->request->get('token'))) {
+            $this->em->remove($productVariant);
+            $this->em->flush();
+
+            $this->addFlash('success', 'ProductVariant supprimer avec succes');
+        } else {
+            $this->addFlash('error', 'Invalide token CSRF');
+        }
+
+        return $this->redirectToRoute('admin.productVariant.index');
     }
 }
